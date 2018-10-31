d286508f683f8786855377162473da489ca133ba
ShoeiChing <shoeiching.chu@pslgroup.com>
2018-10-31 11:27:14 +0800

    condition

---
+++ condition.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 31 11:18:33 2018
+
+@author: shoeiching.chu
+"""
+
+answer = input("Would you like express shipping?")
+if answer == "yes" :
+    print("That will be an extra $10")
+
+    print("Thank You")
+    
