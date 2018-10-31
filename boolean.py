4a3ea362e45465b1fbd93942ea1213d83f0cf3b1
ShoeiChing <shoeiching.chu@pslgroup.com>
2018-10-31 12:00:14 +0800

    condition exercise

---
+++ boolean.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 31 11:44:13 2018
+
+@author: shoeiching.chu
+"""
+
+deposit = input("How much would you like to deposit? ")
+if float(deposit) > 100 :
+    freeToaster=True
+    
+if freeToaster:  
+    print("Enjoy your toaster!")
+print ("Have a nice day")        
--- condition.py
+++ condition.py
@@ -5,9 +5,19 @@ Created on Wed Oct 31 11:18:33 2018
 @author: shoeiching.chu
 """
 
-answer = input("Would you like express shipping?")
+answer = input("Would you like express shipping? (yes/no):").lower()
+shippingSelected = False
+
 if answer == "yes" :
     print("That will be an extra $10")
-
-    print("Thank You")
+    shippingSelected = True
+else:
+    print("Standard Shipping selected..")  
+ 
+if(shippingSelected) :
+      print("Thank you for selecting Express Shipping!!")          
+        
+    
+    
+    
     
