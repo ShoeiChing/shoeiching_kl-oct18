1ed5e2fc0e96a3ea90e64684e27a583a195dfc12
ShoeiChing <shoeiching.chu@pslgroup.com>
2018-10-31 10:26:38 +0800

    date file added

---
+++ datetime.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 31 10:11:40 2018
+
+@author: shoeiching.chu
+"""
+import datetime
+
+currentDate = datetime.date.today()
+
+print (currentDate)
+print (currentDate.year)
+print (currentDate.month)
+print (currentDate.day)
+
+#print (currentDate.strftime('%d %b,%Y'))
+
+print (currentDate.strftime('%A %d %B,%Y'))
