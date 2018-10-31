7f24e6690678224f05eec4832fdc72c53ce48203
ShoeiChing <shoeiching.chu@pslgroup.com>
2018-10-31 11:06:07 +0800

    date file 2

---
+++ dateutil.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 31 10:11:40 2018
+
+@author: shoeiching.chu
+"""
+import datetime
+
+currentDate = datetime.date.today()
+
+print (currentDate)
+print (currentDate.year)
+print (currentDate.month)
+print (currentDate.day)
+
+#print (currentDate.strftime('%d %b,%Y'))
+
+print (currentDate.strftime('%A %d %B,%Y'))
+
+print (currentDate.strftime
+('Please attend our event %A, %B %d in the year %Y'))
+
+print ("Timedelta 15 days :")
+print (currentDate + datetime.timedelta(days=15))
+print ("Timedelta 15 hours :")
+print (currentDate + datetime.timedelta(hours=15))
