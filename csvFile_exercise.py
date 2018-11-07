5d12e1496caba6ffa8d51cab2b0887d9dff1736f
ShoeiChing <shoeiching.chu@pslgroup.com>
2018-11-07 10:34:40 +0800

    csvExercise

---
+++ csvFile_exercise.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  7 10:29:15 2018
+
+@author: shoeiching.chu
+"""
+
+import csv
+
+fileName = "demo.csv"
+accessMode = "r"
+
+with open(fileName, accessMode) as myCSVFile:
+    dataFromFile = csv.reader(myCSVFile)
+
+    for row in dataFromFile :
+        print(row)
